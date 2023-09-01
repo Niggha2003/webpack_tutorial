724be32439ea3fe5b38b265fdb482f543a808359
Niggha2003 <nguyennghia241003@gmail.com>
2023-09-01 22:13:35 +0700

    xóa các thư viện và cú pháp, chuyển về ban đầu cho bài học sau

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.csv';
-import Notes from './data.xml'
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
-console.log(json)
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-  
-    element.appendChild(myIcon);
-
-    console.log(Data[0]);
-    console.log(Notes);
 
     return element;
   }
